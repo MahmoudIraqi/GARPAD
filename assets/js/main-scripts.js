6fbc8150829f1a8bcae848d51d187d8bd0189004
Mahmoud el Sayed <mahmoud.elsayed@simon-kucher.com>
2024-11-23 17:02:32 +0200

    test navigation

--- assets/js/main-scripts.js
+++ assets/js/main-scripts.js
@@ -20,9 +20,6 @@ function setupNavigation() {
 
   // Load content for the default page
   const currentPath = window.location.pathname;
-  const currentPathHash = window.location.hash;
-  console.log('currentPath',currentPath)
-  console.log('currentPathHash',currentPathHash)
   const contentMap = {
     '#main': './pages/main.html',
     '#about': './pages/about.html'
